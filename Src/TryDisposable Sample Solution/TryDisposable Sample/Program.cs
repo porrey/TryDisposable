a46637714d3b10abfc9a105709353cc4d62f8ac1
Daniel Porrey <porrd0bl@prscoad.com>
2020-01-25 09:17:04 -0600

    Clean-up.

---
+++ Src/TryDisposable Sample Solution/TryDisposable Sample/Program.cs
@@ -0,0 +1,69 @@
+﻿// ***
+// *** Copyright(C) 2019-2020, Daniel M. Porrey. All rights reserved.
+// *** 
+// *** This program is free software: you can redistribute it and/or modify
+// *** it under the terms of the GNU Lesser General Public License as published
+// *** by the Free Software Foundation, either version 3 of the License, or
+// *** (at your option) any later version.
+// *** 
+// *** This program is distributed in the hope that it will be useful,
+// *** but WITHOUT ANY WARRANTY; without even the implied warranty of
+// *** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// *** GNU Lesser General Public License for more details.
+// *** 
+// *** You should have received a copy of the GNU Lesser General Public License
+// *** along with this program. If not, see http://www.gnu.org/licenses/.
+// *** 
+using System;
+using System.Threading.Tasks;
+
+#pragma warning disable DF0010
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			// ***
+			// *** An object that cannot be disposed.
+			// ***
+			object obj = new object();
+			await obj.TryDisposeAsync();
+
+			// ***
+			// *** An object that can be disposed.
+			// ***
+			ISomeThing someThing = new SomeThing();
+			await someThing.TryDisposeAsync();
+
+			// ***
+			// *** Wrap the ITemporaryFolder in a using statement.
+			// ***
+			ITemporaryFolder tempFolder1 = TemporaryFolderFactory.Create1();
+
+			using (ITryDisposable<ITemporaryFolder> disposableTempFolder = TryDisposableFactory.Create(tempFolder1))
+			{
+				string path = disposableTempFolder.Instance.Path;
+
+				// ***
+				// *** If tempFolder is disposable, it will get disposed, otherwise it will be ignored.
+				// ***
+			}
+
+			// ***
+			// *** Wrap the ITemporaryFolder in a using statement (non generic interface).
+			// ***
+			ITemporaryFolder tempFolder2 = TemporaryFolderFactory.Create2();
+
+			using (ITryDisposable disposableTempFolder = TryDisposableFactory.Create(tempFolder2))
+			{
+				// ***
+				// *** If tempFolder is disposable, it will get disposed, otherwise it will be ignored.
+				// ***
+			}
+		}
+	}
+}
+
+#pragma warning restore DF0010
---
+++ Src/TryDisposable Sample Solution/TryDisposable Sample/TemporaryFolder.cs
@@ -0,0 +1,57 @@
+﻿// ***
+// *** Copyright(C) 2019-2020, Daniel M. Porrey. All rights reserved.
+// *** 
+// *** This program is free software: you can redistribute it and/or modify
+// *** it under the terms of the GNU Lesser General Public License as published
+// *** by the Free Software Foundation, either version 3 of the License, or
+// *** (at your option) any later version.
+// *** 
+// *** This program is distributed in the hope that it will be useful,
+// *** but WITHOUT ANY WARRANTY; without even the implied warranty of
+// *** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// *** GNU Lesser General Public License for more details.
+// *** 
+// *** You should have received a copy of the GNU Lesser General Public License
+// *** along with this program. If not, see http://www.gnu.org/licenses/.
+// *** 
+using System;
+
+#pragma warning disable DF0100
+
+namespace ConsoleApp1
+{
+	public interface ITemporaryFolder
+	{
+		string Path { get; set; }
+	}
+
+	public class TemporaryFolder1: DisposableObject, ITemporaryFolder
+	{
+		public string Path { get; set; }
+
+		protected override void OnDisposeManagedObjects()
+		{
+			base.OnDisposeManagedObjects();
+		}
+	}
+
+	public class TemporaryFolder2 : ITemporaryFolder
+	{
+		public string Path { get; set; }
+	}
+
+	public static class TemporaryFolderFactory
+	{
+		public static ITemporaryFolder Create1()
+		{
+			return new TemporaryFolder1();
+		}
+
+		public static ITemporaryFolder Create2()
+		{
+			return new TemporaryFolder2();
+		}
+	}
+}
+
+#pragma warning restore DF0100
